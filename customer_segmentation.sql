97847d4819a82e1a98bc8cf1bac4e09a41506990
VladaInd <vlada.indiukova@gmail.com>
2024-10-06 18:37:27 +0200

    Customer Segmentation by Account Balance

---
+++ customer_segmentation.sql
@@ -0,0 +1,8 @@
+select id,balance,
+case
+    when balance < 10000 then 'Low'
+    when balance between 10000 and 100000 then 'Medium'
+    when balance > 100000 then 'High'
+        end as balance_segment
+from banking_data_analysis
+order by balance desc
