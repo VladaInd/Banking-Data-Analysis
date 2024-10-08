98ca65e9b97ea6ed4f676c66b74e00a4e9ce9fcd
VladaInd <vlada.indiukova@gmail.com>
2024-10-08 16:43:48 +0200

    Format file

--- comprehensive_banking_analysis.sql
+++ comprehensive_banking_analysis.sql
@@ -1,38 +1,41 @@
 -- Customer Segmentation by Account Balance
 
-select id,balance,
-case
-    when balance < 10000 then 'Low'
-    when balance between 10000 and 100000 then 'Medium'
-    when balance > 100000 then 'High'
-        end as balance_segment
+select id,
+       balance,
+       case
+           when balance < 10000 then 'Low'
+           when balance between 10000 and 100000 then 'Medium'
+           when balance > 100000 then 'High'
+           end as balance_segment
 from banking_data_analysis
 order by balance desc;
 
 --Customer Segmentation by Credit Score
 
 select credit_score,
-case
-     when credit_score < 600 then 'Poor'
-     when credit_score between 600 and 750 then 'Fair'
-     else 'Excellent'
-   end credit_segment
+       case
+           when credit_score < 600 then 'Poor'
+           when credit_score between 600 and 750 then 'Fair'
+           else 'Excellent'
+           end credit_segment
 from banking_data_analysis;
 
 --Loan Value Ration Analysis
 
 select account_type,
-  round(avg(loan_amount/(loan_amount+balance)),2) *100 as avg_ltv,
-    round(max(loan_amount/(loan_amount+balance)),2)*100 as max_ltv,
-    round(min(loan_amount/(loan_amount+balance)),2)*100 as min_ltv
+       round(avg(loan_amount / (loan_amount + balance)), 2) * 100 as avg_ltv,
+       round(max(loan_amount / (loan_amount + balance)), 2) * 100 as max_ltv,
+       round(min(loan_amount / (loan_amount + balance)), 2) * 100 as min_ltv
 from banking_data_analysis
- where loan_amount> 0 and balance >0
+where loan_amount > 0
+  and balance > 0
 group by account_type
 order by account_type;
 
 -- Top 10 Customers With The Highest Loan Amount
 
-select id,loan_amount from banking_data_analysis
+select id, loan_amount
+from banking_data_analysis
 order by loan_amount desc
 limit 10;
 
@@ -46,13 +49,14 @@ select id,
        balance,
        ROUND((balance / credit_limit) * 100, 2) as credit_limit_utilization,
        case
-        when(balance/credit_limit)*100 > 30 then'Low'
-        when(balance/credit_limit)*100 between 30 and 70 then'Medium'
-        when(balance/credit_limit)*100 <70 then 'High'
-        else 'High'
-     end as utilization_category
+           when (balance / credit_limit) * 100 > 30 then 'Low'
+           when (balance / credit_limit) * 100 between 30 and 70 then 'Medium'
+           when (balance / credit_limit) * 100 < 70 then 'High'
+           else 'High'
+           end                                  as utilization_category
 from banking_data_analysis
-where credit_limit > 0 and balance > 0
+where credit_limit > 0
+  and balance > 0
 order by utilization_category desc;
 
 --The Most Popular Account Types And Loan Products
